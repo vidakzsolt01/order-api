69f41143d0085291ad1a899faab30874de442d34
Vidák Zsolt <vidak.zs@gmail.com>
2020-12-07 06:31:42 +0100

    add productItemList() test to CartTest

--- src/test/java/hu/gov/allamkincstar/training/javasebsc/orderapi/order/CartTest.java
+++ src/test/java/hu/gov/allamkincstar/training/javasebsc/orderapi/order/CartTest.java
@@ -15,6 +15,7 @@ class CartTest extends Container {
     static Product prod1 = new Product("111111", "Termék-1", 1000, 27);
     static Product prod2 = new Product("222222", "Termék-2", 2000, 5);
     static Product prod3 = new Product("333333", "Termék-3", 2000, 12);
+    static Product prod4 = new Product("444444", "Termék-4", 10, 27);
     static Stock stock = new Stock();
     Cart cart = new Cart();
 
@@ -31,6 +32,35 @@ class CartTest extends Container {
 
     @Test
     void productItemList(){
+        // 0 termék van a listában
+        assertEquals(0, cart.productItemList().size());
+        try {
+            // hozzáadunk 5 prod1-et
+            cart.addNewProduct(prod1, 5, stock);
+            // 1 termék van a listában
+            assertEquals(1, cart.productItemList().size());
+            // hozzáadunk 10 prod2-t
+            cart.addNewProduct(prod2, 10, stock);
+            // 2 termék van a listában
+            assertEquals(2, cart.productItemList().size());
+            // hozzáadunk további 5 prod1-et
+            cart.addNewProduct(prod1, 5, stock);
+            // továbbra is 2 termék van a listában
+            assertEquals(2, cart.productItemList().size());
+            cart.addNewProduct(prod3, 10, stock);
+            // az 1. termékből 10 van a kosárlistában
+            assertEquals(10, ((OrderItem)cart.productItemList().get(0)).getQuantity());
+            stock.depositProduct(prod4,120);
+            cart.addNewProduct(prod4, 10, stock);
+            assertEquals(4, cart.productItemList().size());
+            cart.productItemList().add(new OrderItem(prod1, 10));
+            assertEquals(4, cart.productItemList().size());
+            assertEquals(10, ((OrderItem)cart.productItemList().get(3)).getQuantity());
+            ((OrderItem)cart.productItemList().get(3)).increaseQuantity(10);
+            assertEquals(10, ((OrderItem)cart.productItemList().get(3)).getQuantity());
+        } catch (NotEnoughItemException | InvalidBookArgumentException | InvalidIncreaseArgumentException | ItemExistsWithNameException | ItemExistsWithItemNumberException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
